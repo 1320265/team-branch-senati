80a89413ea747abc8380f4e5b7fcfd864e2437ca
carlos ccapa <ccapahuanccojuancarlos@gmail.com>
2025-11-19 16:53:19 -0500

    Agregue nuevo codigo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,1 @@
-document.getElementById("btn-info").addEventListener("click", () => {
-    alert("Esta página fue creada por Cappa en la rama student3.");
-});
+console.log("Página cargada correctamente.");
